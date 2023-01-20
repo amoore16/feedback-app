4bfa242567e83e8098748500a2a9c9a9b94f9eb3
alex <amoore16@gmail.com>
2023-01-20 10:06:48 -0700

    Added links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import Card from '../components/shared/Card'
 
 type Props = {}
@@ -11,7 +11,7 @@ const About = (props: Props) => {
             <h1>About This Project</h1>
             <p>This is a React app to leave feedback for a product or service</p>
             <p>Version: 1.0.0</p>
-            <a href='/'>Back to Home</a>
+            <Link to='/'>Back to Home</Link>
         </div>
     </Card>
   )
